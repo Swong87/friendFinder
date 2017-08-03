4ee892a5764c2d05541c69e1c69e825760fbb8b1
Swong87 <8swordz@gmail.com>
2017-08-03 18:22:45 -0400

    cleaning

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,7 +20,7 @@ module.exports = function (app){
 
 	  	// Compares scores from every person in the friends array with the new user
 	  	for (var i = 0; i < (friends.length); i++){
-	  		console.log(compare(arrayOne, friends[i].scores));
+	  		// console.log(compare(arrayOne, friends[i].scores));
 	  		var matchScore = compare(arrayOne, friends[i].scores);
 	  		// Pushes each users' match score to the user into the scoreArray
 	  		scoreArray.push(matchScore);
@@ -53,7 +53,6 @@ function compare(array1, array2){
 		var matchNum = Math.abs(array1[i] - array2[i]);
 		// Pushes each comparison to the resultArray
 		resultArray.push(matchNum);
-
 	}
 	// Stores the total match score in a variable
 	var totalDifference = resultArray.reduce(add,0);
