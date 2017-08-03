73d75ca8bea6ee0c6139afb0a9db28737a1f8be1
Swong87 <8swordz@gmail.com>
2017-08-02 22:11:13 -0400

    cleaned up

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,3 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
 var path = require("path");
 
 var friends = [{
