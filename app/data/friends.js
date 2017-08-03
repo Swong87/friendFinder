3f8946dcb7fc01a5db54eb3b435113c3709eaa4d
Swong87 <8swordz@gmail.com>
2017-08-02 22:12:44 -0400

    fixed {

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,6 @@
 var path = require("path");
 
 var friends = [{
-{
   name: "Stephen Wong",
   photo: "https://scontent.ftpa1-1.fna.fbcdn.net/v/t1.0-9/19029709_10106727256645602_761931637432358873_n.jpg?oh=49d87385c3d1233485f02c0abf330995&oe=5A31097E",
   scores: [2,5,4,5,5,5,5,4,4,2]
